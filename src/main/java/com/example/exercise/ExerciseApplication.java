1b2a5334efa7df22793f0e756eb68a60aab401aa
David Badiola <dpebadiola@gmail.com>
2021-03-23 23:24:44 +0800

    initial commit

---
+++ src/main/java/com/example/exercise/ExerciseApplication.java
@@ -0,0 +1,17 @@
+package com.example.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseApplication {
+
+	// TODO: cost configuration on given table parameters
+	// TODO: voucher on provided swagger
+	// TODO: dynamic cost configuration
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseApplication.class, args);
+	}
+
+}
